95d826cca52c834355f585cf80f99506b0df8e7e
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2021-05-04 17:24:05 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
   const [ city, setCity ] = useState("Stockholm");
   const GET_WEATHER = gql`
   {
-    getCityByName(name: "${city}") {
+    getCity(name: "${city}") {
       name
       country
       weather {
@@ -35,9 +35,11 @@ function App() {
     }
   }
   `
-
+  // STATES
   const { loading, error, data } = useQuery(GET_WEATHER);
   const [ weatherSearch, setWeatherSearch ] = useState("");
+
+  // ERROR & LOADING
   if (error) return (
     <div className="error-container">
       <h1 className="error-message">Nope, something went wrong</h1>
@@ -46,8 +48,9 @@ function App() {
       </button>
     </div>
   );
-  if (loading) return <h1 style={{textAlign: "center", color: "white"}}>Loading...</h1>;
+  if (loading) return <h4 className="error-message">Loading...</h4>;
 
+  // SEARCH-HANDLER
   const onSearch = evt => {
     if (evt.key === "Enter" && !weatherSearch <= 0) {
       setCity(weatherSearch)
@@ -56,7 +59,7 @@ function App() {
     }
   }
 
-
+  // DATE
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let date = d.getDate();
@@ -73,45 +76,43 @@ function App() {
     )
   }
 
-  const sunOrRainImage = data.getCityByName.weather.temperature.actual > 16 ? Sun : Rain;
-  const blueOrYellowLogo = data.getCityByName.weather.temperature.actual > 16 ? KYellow : KBlue;
-  const temperature = Math.round(data.getCityByName.weather.temperature.actual);
-  const cityNameSlice = data.getCityByName.name.length > 13 ? data.getCityByName.name.slice(0, 13) + "..." : data.getCityByName.name;
+  // QUERY IF-STATEMENTS
+  const sunOrRainImage = data.getCity.weather.temperature.actual > 16 ? Sun : Rain;
+  const coldOrWarmLogo = data.getCity.weather.temperature.actual > 16 ? KYellow : KBlue;
+  const temperature = Math.round(data.getCity.weather.temperature.actual);
+  const cityNameSlice = data.getCity.name.length > 13 ? data.getCity.name.slice(0, 13) + "..." : data.getCity.name;
 
   return (
     <div className="App">
       <div className="container">
-      <img src={blueOrYellowLogo} alt="sun" width="100px" height="100px"></img>
-        <div className="search-box">
-          <input
-            type="text"
-            className="search-bar"
-            placeholder="Search for a city"
-            onChange={e => setWeatherSearch(e.target.value)}
-            onKeyPress={onSearch}
-          />
-        </div>
-        <div className={data.getCityByName.weather.temperature.actual > 16 ? 'card-warm' : 'card-cold'}>
-          {(typeof data.getCityByName != "undefined") ? (
+        <img className="logo" src={coldOrWarmLogo} alt="logo"></img>
+          <div className="search-container">
+            <input
+              type="text"
+              className="search-bar"
+              placeholder="Search for a city"
+              onChange={e => setWeatherSearch(e.target.value)}
+              onKeyPress={onSearch}
+            />
+          </div>
+          <div className={data.getCity.weather.temperature.actual > 16 ? 'card-warm' : 'card-cold'}>
             <div className="cardDetails">
               <div className="weather-details">
                 {dateBuilder(new Date())}
-                <img src={sunOrRainImage} alt="sun"></img>
-                <div className="temperature">{temperature}°</div>
-                <div className="condition">{data.getCityByName.weather.summary.title.toUpperCase()}</div>
+                <img src={sunOrRainImage} alt="weather condition"></img>
+                <p className="temperature">{temperature}°</p>
+                <p className="condition">{data.getCity.weather.summary.title.toUpperCase()}</p>
               </div>
               <div className="city-container">
                 <div className="city-details">
                   <p className="city-name">{cityNameSlice.toUpperCase()}</p>
-                  <p className="country">{data.getCityByName.country}</p>
+                  <p className="country">{data.getCity.country}</p>
                 </div>
               </div>
             </div>
-          ) : ('')}
         </div>
       </div>
     </div>
-    
   )
 }
 
